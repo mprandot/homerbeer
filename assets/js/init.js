2e75657644e216902ab99348253fa737633f7396
marcio <mprandot@gmail.com>
2017-01-10 22:39:31 -0200

    Add newsletter modal, sale-off products

--- assets/js/init.js
+++ assets/js/init.js
@@ -6,10 +6,44 @@ var HomersObject = {};
 
 HomersObject.init = function () {
 
+    var setNewsletterStorage = function () {
+        localStorage.setItem('newsletter', 'true');
+    };
+
+    var openModal = function () {
+        $("#newsletter-modal").modal();
+    };
+
+    var closeModal = function () {
+        $("#newsletter-modal").modal('hide');
+    };
+
+
     return {
         pluginInit: function () {
             $('.slick-carousel').slick();
+        },
+
+        newsletterInit: function () {
+            $('body').mouseleave(function () {
+                if (localStorage.getItem('newsletter') != 'true') {
+                    openModal();
+                    setNewsletterStorage();
+                }
+            });
+
+            $("#newsletter-modal").find(".btn.btn-success").click(function () {
+
+                if ($('input[name=newsletter_email]').val().length > 0) {
+                    alert('Email cadastrado!');
+                    closeModal();
+                }
+            });
+
+
+            //Reset : localStorage.setItem('newsletter',null)
         }
+
     }
 
 }();
@@ -17,4 +51,5 @@ HomersObject.init = function () {
 
 $(function () {
     HomersObject.init.pluginInit();
+    HomersObject.init.newsletterInit();
 });
